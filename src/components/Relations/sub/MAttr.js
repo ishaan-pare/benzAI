d064ea9e82a3c0d0d6fc6057d67e09631415f343
ishaan-pare <ishaan.pare2020@vitbhopal.ac.in>
2022-05-27 18:27:59 +0530

    added progress in MAttributes

--- src/components/Relations/sub/MAttr.js
+++ src/components/Relations/sub/MAttr.js
@@ -7,47 +7,55 @@ import {
 } from "@mui/material"
 import { Container } from "@mui/system";
 import axios from "axios";
+import Progress from "../../Progress/Progress";
 const style = {
     width: '100%',
     maxWidth: 360,
     bgcolor: 'background.paper',
 };
 
-const preciseFloat = (num)=>{
-    num = num*100;
-    num = num.toString().slice(0,5);
+const preciseFloat = (num) => {
+    num = num * 100;
+    num = num.toString().slice(0, 5);
     return num;
 }
 
 export default function Mattr() {
-    const [attr, setAttr] = useState(["Sales","Price", "Width"]);
+    const [attr, setAttr] = useState(["Sales", "Price", "Width"]);
+    const [load, setLoad] = useState(0);
 
-    useEffect(()=>{
+    useEffect(() => {
+        setLoad(1);
+
         axios.get("https://carfeaturesanalysis.herokuapp.com/m_attr")
-                    .then((res)=>{
-                        setAttr(res.data)
+            .then((res) => {
+                setAttr(res.data)
 
-                    })
+            })
+            .then(() => setLoad(0));
 
-    }, [attr]);
+    }, []);
 
     return (
         <Container style={{ "padding": "12px" }} maxWidth="xxl">
             <h2>Most Dependent features</h2><br />
-            <List sx={style} component="nav" aria-label="mailbox folders">
-                {
-                    Object.keys(attr).reverse().map((key) => {
-                        return (
-                            <>
-                                <ListItem button > 
-                                    <ListItemText sx={{fontSize:"sm", display: 'list-item'}} primary={attr[key][0]+" and "+attr[key][1]} secondary={"have "+preciseFloat(key)+"% dependencies"}/>
-                                </ListItem>
-                                <Divider />
-                            </>
-                        )
-                    })
-                }
-            </List>
+            {
+                load === 1 ? <Progress /> :
+                    <List sx={style} component="nav" aria-label="mailbox folders">
+                        {
+                            Object.keys(attr).reverse().map((key) => {
+                                return (
+                                    <>
+                                        <ListItem button >
+                                            <ListItemText sx={{ fontSize: "sm", display: 'list-item' }} primary={attr[key][0] + " and " + attr[key][1]} secondary={"have " + preciseFloat(key) + "% dependencies"} />
+                                        </ListItem>
+                                        <Divider />
+                                    </>
+                                )
+                            })
+                        }
+                    </List>
+            }
         </Container>
 
     );
